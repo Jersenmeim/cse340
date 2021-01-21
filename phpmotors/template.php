aa07d13cd29a14cf5b729de1b6a88522875d844f
Jersen Meim <51324287+Jersenmeim@users.noreply.github.com>
2021-01-21 15:07:48 +0800

    enhancement2

---
+++ phpmotors/template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="bg">
+        <div class="header">
+            <img src="../phpmotors/images/site/logo.png" alt="phpmotor_logo">
+            <div class="myaccount" >
+                <a href="#">My Account</a>
+            </div>
+        </div>
+       <?php    
+            include 'nav.php';
+       ?>
+       
+            <h2>Content Here</h2>
+
+        <?php    
+            include 'footer.php';
+        ?>
+        
+    </div>
+   
+    <script src="js/script.js"></script>
+</body>
+
+</html>
